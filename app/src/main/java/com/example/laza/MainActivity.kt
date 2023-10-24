7f9afd800e4b5a27904f0a03394cb1eda94940d2
ahmed.tantawy <ahmed.tantawy@sewedy.com.eg>
2023-10-24 13:58:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/laza/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.laza
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.laza.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
