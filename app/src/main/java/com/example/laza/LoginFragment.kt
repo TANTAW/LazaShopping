07ad0b0364d8ffd1b52e2b294afd7ca40144e643
ahmed.tantawy <ahmed.tantawy@sewedy.com.eg>
2023-10-25 11:54:50 +0200

    created login forgot password and sign up screens

---
+++ app/src/main/java/com/example/laza/LoginFragment.kt
@@ -0,0 +1,78 @@
+package com.example.laza
+
+import android.graphics.Color
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.findFragment
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private lateinit var textview: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        val string =
+            "By connecting your account confirm that you agree with our Terms and Conditions"
+        val text =
+            SpannableString(string)
+        val foregroundColor = ForegroundColorSpan(Color.BLACK)
+        text.setSpan(
+            foregroundColor,
+            string.lastIndexOf("Terms"),
+            string.length - 1,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        textview = requireView().findViewById(R.id.tv_terms_and_condition)
+        textview.text = text
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment LoginFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            LoginFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
