cfb59e5f9cbbf4087a4e770d524b28feeb3559fa
ahmed.habib <ahmed.habib@sewedy.com.eg>
2023-10-28 17:00:20 +0200

    Working into product details screen

---
+++ app/src/main/java/com/example/laza/dashboard/productDetails/ProductDetailsFragment.kt
@@ -0,0 +1,20 @@
+package com.example.laza.dashboard.productDetails
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.laza.R
+import com.example.laza.databinding.FragmentProductDetailsBinding
+import com.example.laza.utils.BaseFragment
+
+
+class ProductDetailsFragment :
+    BaseFragment<FragmentProductDetailsBinding>(FragmentProductDetailsBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
