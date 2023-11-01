2c4d4548d3d3b1eacc3a670f1a4541dfcb9e458c
ahmed.habib <ahmed.habib@sewedy.com.eg>
2023-11-01 12:08:35 +0200

    Finished payment screens

---
+++ app/src/main/java/com/example/laza/payment/AddNewCardFragment.kt
@@ -0,0 +1,14 @@
+package com.example.laza.payment
+
+import android.os.Bundle
+import android.view.View
+import com.example.laza.core.BaseFragment
+import com.example.laza.databinding.FragmentAddNewCardBinding
+
+class AddNewCardFragment :
+    BaseFragment<FragmentAddNewCardBinding>(FragmentAddNewCardBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
