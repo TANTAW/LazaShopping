11aa6137ce1b6c916808eef402fb96931e29b6c4
ahmed.tantawy <ahmed.tantawy@sewedy.com.eg>
2023-11-12 21:00:18 +0200

    added menu and navigation

--- app/src/main/java/com/example/laza/MainActivity.kt
+++ app/src/main/java/com/example/laza/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.laza
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import androidx.navigation.Navigation
 import com.example.laza.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        onClick()
+    }
+
+    private fun onClick() {
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+
+                R.id.homeID -> {
+                    Navigation.findNavController(binding.dashboardNavGraph).navigate(R.id.homeFragment)
+                    binding.bottomNav.menu.findItem(R.id.homeID).icon?.setVisible(false, true)
+                }
+
+                R.id.favoriteID -> {
+                    Navigation.findNavController(binding.dashboardNavGraph).navigate(R.id.favoriteFragment)
+                    binding.bottomNav.menu.findItem(R.id.favoriteID).icon?.setVisible(false, true)
+                }
+
+                R.id.wishlistID -> {
+                    Navigation.findNavController(binding.dashboardNavGraph).navigate(R.id.wishListFragment)
+                    binding.bottomNav.menu.findItem(R.id.wishlistID).icon?.setVisible(false, true)
+                }
+
+                R.id.paymentID -> {
+                    Navigation.findNavController(binding.dashboardNavGraph).navigate(R.id.paymentFragment)
+                    binding.bottomNav.menu.findItem(R.id.paymentID).icon?.setVisible(false, true)
+                }
+            }
+
+            true
+        }
     }
 }
